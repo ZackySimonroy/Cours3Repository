b452f14da7fcbc18b8ff6e12a7bded7fbc79bd07
B533P-03\533 <simoroy94@gmail.com>
2018-08-27 16:54:06 -0400

    cours3 Player and  gun

---
+++ Cours 3/Assets/Script/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        Vector3 ajustedMouvement = transform.TransformDirection(Vector3.forward);
+        rigidBody.AddForce(ajustedMouvement * 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Cours 3/Assets/Script/FireGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour {
+    [SerializeField] GameObject bullet;
+    [SerializeField] float fireRate = 1;
+    [SerializeField] GameObject bulletEmitter;
+
+    private float timerLastShot = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerLastShot += Time.deltaTime;
+        if(timerLastShot > fireRate)
+        {
+            var fire1 = Input.GetAxis("Fire1");
+            if (fire1 > 0)
+            {
+                Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation);
+                timerLastShot = 0;
+            }
+        }
+	}
+}
